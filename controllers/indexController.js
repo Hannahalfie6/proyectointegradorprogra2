ddfd17d03cbbd603cefea8bff52b9c002273a201
Hannahalfie6 <halfie@udesa.edu.ar>
2025-09-04 16:48:46 -0300

    Comienzo de Punto 1 - Migración a Node y Express

---
+++ controllers/indexController.js
@@ -0,0 +1,11 @@
+let data = require('../localData/index')
+
+let indexController = {
+    home: function(req,res) {
+        
+        //return res.send(data)
+        return res.render('index', {data: data})
+    }
+}
+
+module.exports = indexController;
---
+++ localData/index.js
@@ -0,0 +1,230 @@
+let data = {
+    usuario: {
+
+    },
+    productos: [
+        {
+            nombre: 'Termo Quencher 30OZ',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: './'
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            comentarios: [
+                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                },
+                                {
+                    nombre: '',
+                    texto: '',
+                    foto: ''
+                }
+            ]
+        },       
+
+    ]
+}
+
+module.exports = data;
