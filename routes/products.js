479be9e0c627654c8de31ef5d4aedbde1bdb6dfd
Hannahalfie6 <halfie@udesa.edu.ar>
2025-09-05 16:30:42 -0300

    router products

---
+++ routes/products.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+let productsController = require('../controllers/productsController')
+
+
+/* GET home page. */
+router.get('/', );
+
+module.exports = router;
