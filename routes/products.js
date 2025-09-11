17c01dd7c360f024f026c5b6d185acac05ee50ec
Hannahalfie6 <halfie@udesa.edu.ar>
2025-09-11 11:00:03 -0300

    formulario login, y todas las vistas de head, header y footer

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 let productsController = require('../controllers/productsController');
 
 /* GET home page. */
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
 let usersController = require('../controllers/usersController');
+
 /* GET users listing. */
 router.get('/profile', usersController.profile);
-
-
 router.get('/login', usersController.login);
-
-
 router.get('/register', usersController.register);
-
 
 
 module.exports = router;
