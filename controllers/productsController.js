b8980fb19f05894e8fd6d76814be3de7fdad7021
magui-sanchez <sanchezma@udesa.edu.ar>
2025-09-23 00:21:44 -0300

    cambie en index.ejs el <% para que reciba el idProducto y cambie el productController para que recorra bien y muestre la vista del detalle. Ahora la url del navegador muestra el id del producto y no el indice del array

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,19 @@
 let data = require('../localData/index'); 
 let productosController = { 
     detalle: function(req, res){ 
-        let producto = data.productos[req.params.id]; 
+        let producto = {}
+        for (let i = 0; i < data.productos.length; i++) {
+            if (data.productos[i].idProducto == req.params.id) {
+                producto = data.productos[i]
+            }
+        }
         return res.render('product', {data: producto, usuario:{ }}) // es estatico por eso 0 
     }, 
     agregar: function(req, res) { 
             return res.render('product-add', {usuario:{ }}) 
     } 
     }; 
+
+
     
 module.exports = productosController;
